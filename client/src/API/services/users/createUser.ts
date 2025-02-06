778e32bae4762910025980d90552ebd6359d9469
KM <itmuminoff@gmail.com>
2025-02-06 12:06:46 +0500

    regme logme

--- client/src/API/services/users/createUser.ts
+++ client/src/API/services/users/createUser.ts
@@ -1,24 +1,5 @@
-interface TData {
-	login: string;
+export interface RegmeProps {
+	username: string;
+	inn: string; //login
+	password: string;
 }
-
-export const createUser = async (newUser: TData): Promise<any> => {
-	try {
-		const response = await fetch("http://localhost:3000/regme", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(newUser),
-		});
-		if (!response.ok) {
-			throw new Error(`Error on createUser`);
-		}
-		console.log(response);
-
-		return response.json();
-	} catch (error) {
-		console.error("Error:", error);
-		throw error;
-	}
-};
