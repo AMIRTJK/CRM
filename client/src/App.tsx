d8f047d43db5e8f3b212444176c94424301c6c64
KM <itmuminoff@gmail.com>
2025-02-07 14:17:12 +0500

    added dynamic bg changer logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router";
-import { lazy, Suspense } from "react";
+import { Route, Routes, useLocation } from "react-router";
+import { lazy, Suspense, useEffect, useRef } from "react";
 import "./App.css";
 import { Loader } from "./UI/Loader/Loader";
 import Header from "./components/Header/Header";
 
 const LazyAuthrozation = lazy(
-  () => import("./routes/Auth/Authorization/Authorization")
+	() => import("./routes/Auth/Authorization/Authorization"),
 );
 const LazyLogMe = lazy(() => import("./routes/Auth/Logme/Logme"));
 const LazyRegMe = lazy(() => import("./routes/Auth/Regme/Regme"));
@@ -14,31 +14,40 @@ const LazyCreateCRM = lazy(() => import("./routes/CRM/Create/CreateCRM"));
 const LazyShowCRM = lazy(() => import("./routes/CRM/Show/ShowCRM"));
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main className="section-offset">
-        <div className="container">
-          <div className="app__content">
-            <Suspense fallback={<Loader />}>
-              <Routes>
-                <Route path="/crm" element={<LazyCrm />} />
-                {/* Доработать КМ */}
-                <Route path="/" element={<LazyAuthrozation />} />
-                <Route path="/auth" element={<LazyAuthrozation />}>
-                  <Route path="regme" element={<LazyRegMe />} />
-                  <Route path="logme" element={<LazyLogMe />} />
-                </Route>
-                <Route path="*" element={<h2>Страница не существует</h2>} />
-                <Route path="/crm/create" element={<LazyCreateCRM />} />
-                <Route path="/crm/show/:id" element={<LazyShowCRM />} />
-              </Routes>
-            </Suspense>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+	const location = useLocation();
+
+	useEffect(() => {
+		if (location.pathname === "/auth/logme") {
+			document.querySelector(".section-offset")?.classList.add("main-bg");
+		} else {
+			document.querySelector(".section-offset")?.classList.remove("main-bg");
+		}
+	}, [location.pathname]);
+	return (
+		<>
+			<Header />
+			<main className="section-offset">
+				<div className="container">
+					<div className="app__content">
+						<Suspense fallback={<Loader />}>
+							<Routes>
+								<Route path="/crm" element={<LazyCrm />} />
+								{/* Доработать КМ */}
+								<Route path="/" element={<LazyAuthrozation />} />
+								<Route path="/auth" element={<LazyAuthrozation />}>
+									<Route path="regme" element={<LazyRegMe />} />
+									<Route path="logme" element={<LazyLogMe />} />
+								</Route>
+								<Route path="*" element={<h2>Страница не существует</h2>} />
+								<Route path="/crm/create" element={<LazyCreateCRM />} />
+								<Route path="/crm/show/:id" element={<LazyShowCRM />} />
+							</Routes>
+						</Suspense>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
