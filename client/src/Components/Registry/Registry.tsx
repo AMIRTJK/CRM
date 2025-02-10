8e3342a70c738e6e360b66a73adcc99099ec7099
AMIRTJK <jsharipovamir@gmail.com>
2025-02-10 17:14:59 +0500

    end table row

--- client/src/Components/Registry/Registry.tsx
+++ client/src/Components/Registry/Registry.tsx
@@ -17,55 +17,81 @@ const Registry = ({ headersProps, rowsProps, status }: TProps) => {
 
   const location = useLocation();
 
-  console.log(location);
+  const navigate = useNavigate();
+
+  const handleRowClick = (rowId: string) => {
+    if (location.pathname === "/crm") {
+      navigate(`/crm/show/${rowId}`); // Навигация на нужный путь
+    }
+  };
 
   return (
-    <div className="flex-table">
-      <div className="flex-table-header">
-        {/* Заголовки */}
-        {headers.map((headers, index) => {
-          return (
-            <p key={index} className="flex-header-item">
-              {headers}
-            </p>
-          );
-        })}
-      </div>
-
-      {/* Строки */}
-      <div className="flex-table-body">
+    <table className="registry">
+      <thead>
+        <tr>
+          {headers.map((headers, index) => {
+            return <th key={index}>{headers}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
         {rows?.map((row: any[], rowIndex: React.Key | null | undefined) => (
-          <Link
+          <tr
             key={rowIndex}
-            to={`${location.pathname === "/crm" ? `/crm/show/${row[0]}` : "#"}`}
+            onClick={() => handleRowClick(row[0])} // Передаем id для навигации
           >
-            {/* Используем id для навигации */}
-            <div className="flex-row">
-              {row.slice(1).map(
-                (
-                  cell,
-                  cellIndex // Пропускаем id, чтобы не отображать его в таблице
-                ) => (
-                  <div key={cellIndex} className="flex-cell">
-                    <p
-                      className={
-                        cell === status.active
-                          ? "active-status"
-                          : cell === status.inactive
-                          ? "inactive-status"
-                          : ""
-                      }
-                    >
-                      {cell}
-                    </p>
-                  </div>
-                )
-              )}
-            </div>
-          </Link>
+            {row.slice(1).map((cell, cellIndex) => (
+              <td key={cellIndex}>
+                <p
+                  className={
+                    cell === status.active
+                      ? "active-status"
+                      : cell === status.inactive
+                      ? "inactive-status"
+                      : ""
+                  }
+                >
+                  {cell}
+                </p>
+              </td>
+            ))}
+          </tr>
         ))}
-      </div>
-    </div>
+      </tbody>
+    </table>
+
+    // <div className="flex-table-body">
+    //   {rows?.map((row: any[], rowIndex: React.Key | null | undefined) => (
+    //     <Link
+    //       key={rowIndex}
+    //       to={`${location.pathname === "/crm" ? `/crm/show/${row[0]}` : "#"}`}
+    //     >
+    //       {/* Используем id для навигации */}
+    //       <div className="flex-row">
+    //         {row.slice(1).map(
+    //           (
+    //             cell,
+    //             cellIndex // Пропускаем id, чтобы не отображать его в таблице
+    //           ) => (
+    //             <div key={cellIndex} className="flex-cell">
+    //               <p
+    //                 className={
+    //                   cell === status.active
+    //                     ? "active-status"
+    //                     : cell === status.inactive
+    //                     ? "inactive-status"
+    //                     : ""
+    //                 }
+    //               >
+    //                 {cell}
+    //               </p>
+    //             </div>
+    //           )
+    //         )}
+    //       </div>
+    //     </Link>
+    //   ))}
+    // </div>
   );
 };
 
